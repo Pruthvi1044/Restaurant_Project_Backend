036f7abdfd649a6dc7445624f739207c0f4d1036
Pruthvi1044 <pruthvirajkhedkar23@gmail.com>
2026-04-18 11:26:34 +0530

    Moved email credentials to env file

--- RestaurantBackend/settings.py
+++ RestaurantBackend/settings.py
@@ -15,6 +15,10 @@ from decouple import config
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -179,7 +183,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'pruthvirajkhedkar23@gmail.com'
-EMAIL_HOST_PASSWORD = 'qmvs feti zbrr icxn'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
